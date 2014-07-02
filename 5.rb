0aa83a0dd1ed9f7541aa5091c931135c1683f78a
veeresh <veeresh@railsfactory.org>
2014-07-02 15:13:11 +0530

    simple calculator

---
+++ 5.rb
@@ -0,0 +1,12 @@
+puts "enetr the two number to be added"
+  n1 = gets.chomp.to_i
+  n2 = gets.chomp.to_i
+  sum=n1 + n2
+  mul=n1 * n2
+  sub= n1- n2
+  div= n1 / n2
+puts "the sum of two intgeger is #{sum}"
+puts "the mulitiplication of two intgeger is #{mul}"
+puts "the subtraction of two intgeger is #{sub}"
+puts "the division of two intgeger is #{div}"
+#division method roundoffs the decimal points to integer like division of 3 and 2 is 1.5 its roundoffs to 1. 
