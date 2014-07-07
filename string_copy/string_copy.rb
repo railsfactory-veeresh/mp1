b807bd901b4fa96ee391abc0d5f6c5c68f97911d
veeresh <veeresh@railsfactory.org>
2014-07-07 20:15:30 +0530

    copying string from source to destination

---
+++ string_copy/string_copy.rb
@@ -0,0 +1,5 @@
+puts "eneter the string to be copied"
+source_string=gets.chomp
+puts "source_string is #{source_string}"
+destination_string=source_string
+puts "string copied to destination_string is #{destination_string}"
