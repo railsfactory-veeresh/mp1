1a02599f5057fc9fdd9ae22828729fa66152379d
veeresh <veeresh@railsfactory.org>
2014-07-07 21:41:31 +0530

    case conversion of given string

---
+++ case_conversion/case_conversion.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char string[] = "Strlwr in C";
+ 
+    printf("%s\n",strlwr(string));
+ 
+    return  0;
+    
+    char string[] = "strupr in c";
+ 
+    printf("%s\n",strupr(string));
+ 
+    return  0;
+}
---
+++ case_conversion/case_conversion.rb
@@ -0,0 +1,7 @@
+
+puts "eneter the string to be converted into upeper case or lower case"
+string1=gets.chomp
+string1=string1.upcase
+puts "the upper case of given string is->#{string1} "
+string1=string1.downcase
+puts "the lower case of given string is -> #{string1}"
