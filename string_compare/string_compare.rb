450a4a999fc4154d703826cd9b906fb9a3b7dbe1
veeresh <veeresh@railsfactory.org>
2014-07-07 20:06:27 +0530

    string comparision

---
+++ string_compare/string_compare.rb
@@ -0,0 +1,9 @@
+puts "eneter the first string"
+string1=gets.chomp
+puts "eneter the second string"
+string2=gets.chomp
+if string1 == string2
+   puts "the given strings are equal"
+else
+   puts "the strings are not equal"
+end
