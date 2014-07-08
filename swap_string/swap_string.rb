89524108628a399af540fdb449d80b66b8b65ea9
veeresh <veeresh@railsfactory.org>
2014-07-08 20:52:44 +0530

    swapping of strings

---
+++ swap_string/swap_string.rb
@@ -0,0 +1,11 @@
+puts "enter the first string to swap"
+string1 = gets.chomp
+puts "enter the second string to swap"
+string2 = gets.chomp
+puts "\n"
+temp = ""
+temp = string1
+string1 = string2
+string2 = temp
+puts string1
+puts string2
